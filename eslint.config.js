13fc71e52a1857582ffe4f6deda9ff98d63591e8
unknown <theevlchannel@gmail.com>
2024-07-05 15:06:38 +0400

    siski

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginVue from "eslint-plugin-vue";
+import pluginPrettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+
+export default [
+  { files: ["*/.{js,mjs,cjs,vue}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs["flat/essential"],
+  pluginPrettier,
+  configPrettier,
+];
